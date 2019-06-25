3f364681157af351268916d50e836537fde00575
salif-04 <syedsalifmoin@gmail.com>
2019-06-26 00:39:04 +0530

    Adds MIC, E-Invoice

--- assets/js/projects.js
+++ assets/js/projects.js
@@ -1,16 +1,28 @@
-// MIC viewer
+// JavaScript30 viewer
 $("#js30").click(function(){
     $("#js30-details").css("display","block");
 });
+// Practical JS viewer
 $("#pjs").click(function(){
     $("#pjs-details").css("display","block");
 });
+// Vapours viewer
 $("#vapours").click(function(){
     $("#vapours-details").css("display","block");
+});
+// E-Invoice JS viewer
+$("#einvoice").click(function(){
+    $("#einvoice-details").css("display","block");
+});
+// MIC viewer
+$("#mic").click(function(){
+    $("#mic-details").css("display","block");
 });
 
 $(".close").click(function(){
     $("#js30-details").css("display","none");
     $("#pjs-details").css("display","none");
     $("#vapours-details").css("display","none");
+    $("#einvoice-details").css("display","none");
+    $("#mic-details").css("display","none");
 });
