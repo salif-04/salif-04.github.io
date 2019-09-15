48878ed4879536e82cc0de0cdcc4866156775de0
salif-04 <syedsalifmoin@gmail.com>
2019-09-15 23:36:28 +0530

    Adds email support

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,5 +1,13 @@
+const display = (headerData, bodyData) => {
+    $('#submit-data-header').text(headerData);
+    $('#submit-data').text(bodyData);
+    $("#submit-details").css("display","block");
+}
+
+// Handles email sending
 $("#submitEmail").click(function(e){
     e.preventDefault();
+
     const data = {
         from_name: $('#forName').val(),
         from_email: $('#forEmail').val(),
@@ -9,12 +17,29 @@ $("#submitEmail").click(function(e){
         phone: $('#forPhone').val()
     };
 
+    // Check for any unfilled input field
     if (!(data.from_name && data.from_email && data.subject && data.message_html && data.address && data.phone)) {
-        $('#submit-data-header').text("Incomplete Form");
-        $('#submit-data').text("Please Fill the Complete Form");
-        $("#submit-details").css("display","block");
+        display('Incomplete Form', 'Please Fill the Complete Form');
     }
-
+    else {
+        display('Sending Email', 'Please wait while we send the email.')
+        $.ajax('https://api-emailjs.herokuapp.com/email', {
+            type: 'POST',
+            data: JSON.stringify(data),
+            contentType: 'application/json',
+        }).done(function(responseData) {
+            if (responseData.status === 200) { // email send successfully
+                display('Email Sent', 'Your email was sent successfully.');
+            }
+            else { // error occured while sending email
+                display('Error Occured', 'An error occured while sending the email. Kindly email to admin at syedsalifmoin@gmail.com');
+            }
+            
+        }).fail(function() {
+            display('Error Occured', 'Cannot send email Currently. Check your Internet Connection or try again later.');
+        })
+    }
+    
     console.log(data);
 });
 
